edb370b245717e84f28f5fee2fb4f8c0723ce5c7
Emil Lau <r09343018@ntu.edu.tw>
2023-02-25 13:18:52 +0800

    process to customised message in delete methodd

--- index.js
+++ index.js
@@ -16,15 +16,34 @@ const product = [
   { name: "女裝", price: "350", stock: "3" },
 ];
 
+const productWithID = product.map((eachProduct, index) => ({
+  ...eachProduct,
+  id: index,
+}));
+console.log(productWithID);
+
+// respond ok to post api
 app.post("/product", (req, res) => {
   console.log(req.body);
   res.json({ message: "ok" });
 });
 
+app.put("/product/:id", (req, res) => {
+  console.log(req.body);
+  console.log(req.params.id);
+  res.json({ message: "ok" });
+});
+
+app.delete("/product/:id", (req, res) => {
+  // product.findIndex(p=>p.id===)
+  console.log(req.params.id);
+  res.json({ message: `Product ` });
+});
+
+// send content when parameter matches "/"
 app.get("/", (req, res) => {
   res.json(product);
 });
-// send content when parameter matches "/"
 
 app.get("/product/:id", (req, res) => {
   res.json(product[parseInt(req.params.id) - 1]);
