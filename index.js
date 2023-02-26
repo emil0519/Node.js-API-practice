1a9fa082ce8581a684e76c2157f74b3c7991a200
Emil Lau <r09343018@ntu.edu.tw>
2023-02-26 14:42:39 +0800

    Connected to mongoDB, but codes are still for local host

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const express = require("express");
 const app = express();
 const port = 3002;
+const mongoose = require("mongoose");
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+const dotenv = require("dotenv");
+dotenv.config();
+const authRoute = require("./routes/auth");
 // app.use(function (req, res) {
 //   res.setHeader("Content-Type", "text/plain");
 //   res.write("you posted:\n");
 //   res.end(JSON.stringify(req.body, null, 2));
 // });
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Connected to database");
+    app.listen(port, () => console.log(`Listening port ${port}`));
+  })
+  .catch((error) => console.log(error));
 
 const product = [
   { name: "洋裝", price: "350", stock: "3" },
@@ -21,6 +33,8 @@ const productWithID = product.map((eachProduct, index) => ({
   id: index,
 }));
 console.log(productWithID);
+
+app.use("/api/auth", authRoute);
 
 // respond ok to post api
 app.post("/product", (req, res) => {
@@ -42,18 +56,13 @@ app.delete("/product/:id", (req, res) => {
   res.json({
     message: `Product ${productWithID[deleteIndex].name} is deleted`,
   });
-  // res.json({
-  //   message: `ok`,
-  // });
 });
 
 // send content when parameter matches "/"
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
   res.json(product);
 });
 
 app.get("/product/:id", (req, res) => {
   res.json(product[parseInt(req.params.id) - 1]);
 });
-
-app.listen(port, () => console.log(`Listening port ${port}`));
